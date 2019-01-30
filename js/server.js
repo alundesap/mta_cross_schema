1125dc1d22e1fd19530c190656cad0b0bf402af9
Andrew Lunde <andrew.lunde@sap.com>
2019-01-30 11:04:20 -0500

    inital commit

---
+++ js/server.js
@@ -0,0 +1,39 @@
+/*eslint no-console: 0, no-unused-vars: 0*/
+"use strict";
+
+var xsjs  = require("@sap/xsjs");
+var xsenv = require("@sap/xsenv");
+var port  = process.env.PORT || 3000;
+
+var options = {
+	anonymous : false, // remove to authenticate calls
+	redirectUrl : "/index.xsjs"
+};
+
+// configure HANA
+try {
+	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
+// configure UAA
+try {
+	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
+// Add SQLCC
+try {
+	options.hana.sqlcc = xsenv.getServices({
+		"xsjs.sqlcc_config":"CROSS_SCHEMA_LOCATIONS"
+	});
+} catch (err) {
+	console.log("[WARN]", err.message);
+}
+
+// start server
+xsjs(options).listen(port);
+
+console.log("Server listening on port %d", port);
